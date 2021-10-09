ee79dba1c2cf86781a0d262d202f2a66c35c5782
Saurav Sahu <mrsauravsahu@outlook.com>
2021-10-10 01:32:49 +0530

    use service name to check health

--- services/documentation/src/routes/index.svelte
+++ services/documentation/src/routes/index.svelte
@@ -1,34 +1,30 @@
 <script context="module">
 	export async function preload() {
-		const services = [
-			`http://${process.env.API_SERVICE_HOST}:${process.env.API_SERVICE_PORT}/`,
-			`http://${process.env.PARSER_SERVICE_HOST}:${process.env.PARSER_SERVICE_PORT}/`
-		]
+		const services = ["api", "parser"];
 
 		const requestPromises = services.map(
-			(serviceUrl) =>
+			(service) =>
 				new Promise(async (resolve) => {
 					let connection = true;
 					let responseJson = {};
 
 					try {
-						const response = await this.fetch(serviceUrl);
+						const response = await this.fetch(`http://${service}`);
 						responseJson = await response.json();
-					}
-					catch(error) {
+					} catch (error) {
 						connection = false;
 					}
 
 					resolve({
-						serviceUrl,
+						serviceUrl: service,
 						connection,
 						body: responseJson,
 					});
 				})
 		);
 
-				const serviceData = await Promise.allSettled(requestPromises);
-				return {serviceData};
+		const serviceData = await Promise.allSettled(requestPromises);
+		return { serviceData };
 	}
 </script>
 
@@ -44,7 +40,9 @@
 	{#each serviceData as service}
 		<li>
 			<div class="service-url">{service.value.serviceUrl}</div>
-			<div>Connection: {service.value.connection ? "Alive": "Could not establish"}</div>
+			<div>
+				Connection: {service.value.connection ? "Alive" : "Could not establish"}
+			</div>
 			<div>Reported Service Name: {service.value.body.name ?? "Unknown"}</div>
 		</li>
 	{/each}

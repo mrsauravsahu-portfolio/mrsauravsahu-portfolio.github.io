2263578a388e96088e4d54bfd87f65b02ab0d248
Saurav Sahu <mrsauravsahu@outlook.com>
2021-10-10 01:33:00 +0530

    update error page for site

---
+++ app/src/routes/__error.svelte
@@ -0,0 +1,27 @@
+<script context="module">
+export function load({ error, status }) {
+		return {
+			props: {
+				error,
+				status
+			}
+		};
+	}
+</script>
+
+<script>
+	export let error;
+	export let status;
+
+	const dev = process.env.NODE_ENV != 'production';
+</script>
+
+<section>
+<h1>whoops, something went wrong!</h1>
+
+{#if dev && error}
+	<div>
+		<p>{error.message}</p>
+	</div>
+{/if}
+</section>

d53d981274c3c2a8e44c6068daea444d61f4992b
Saurav Sahu <mrsauravsahu@outlook.com>
2021-12-04 18:33:20 +0530

    feat: add root title

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,7 +1,13 @@
+<svelte:head>
+	<title>@mrsauravsahu</title>
+</svelte:head>
+
 <section class="home">
 	<div class="bio">
-		<p>software engineer by profession; <br/>
-			photographer, cook and a student at heart.</p>
+		<p>
+			software engineer by profession; <br />
+			photographer, cook and a student at heart.
+		</p>
 	</div>
 </section>
 
@@ -9,7 +15,7 @@
 	.home {
 		width: 100%;
 		display: flex;
-    padding: 0 0.5rem;
+		padding: 0 0.5rem;
 		background: url('/img/mrss-silhouette.svg');
 		background-repeat: no-repeat;
 		background-position: right bottom;

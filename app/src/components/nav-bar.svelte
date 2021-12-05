7c2352b89e9b840799d907ac254a9bab5c183409
Saurav Sahu <mrsauravsahu@outlook.com>
2021-12-05 17:13:03 +0530

    improv: tweak nav bar links

--- app/src/components/nav-bar.svelte
+++ app/src/components/nav-bar.svelte
@@ -7,8 +7,8 @@
 		<img src="/img/brand.svg" alt="brand" />
 	</a>
 	<ul class="navlinks">
-		<li class="navlink"><a href="/#blog">recent blogs</a></li>
 		<li class="navlink"><a href="/#contact">contact</a></li>
+		<li class="navlink"><a href="/blog">blog</a></li>
 		<li class="navlink"><a href="/made-with">made-with</a></li>
 		<li class="navlink"><a href="https://poly.mrsauravsahu.tech">polywork</a></li>
 	</ul>

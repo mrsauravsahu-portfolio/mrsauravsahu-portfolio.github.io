f7cf9b89b701995c9649500f42afcaf0af1f9429
Saurav Sahu <mrsauravsahu@outlook.com>
2022-03-26 03:32:43 +0530

    Feat: Create store service (#5)
    * feat: create store service
    
    * chore: add a wip test rule for store svc
    
    * fix: BUILD missing ','
    
    * fix: use svc name as default build target for store
    
    * improv: use express for store service

---
+++ svc/store/index.ts
@@ -0,0 +1,12 @@
+import {default as Express} from 'express'
+
+const app = Express()
+
+const port = process.env.PORT ? +process.env.PORT : 80
+const host = process.env.HOST ?? '0.0.0.0'
+
+app.use(Express.static('data'))
+
+app.listen(port, host, ( ) => {
+    console.log(`Started server at http://${host}:${port}`)
+})

17476e3432ae74bf6a84b5608b20675d612e8009
Saurav Sahu <mrsauravsahu@outlook.com>
2022-03-26 14:08:08 +0530

    chore: lint fix store svc

--- svc/store/index.ts
+++ svc/store/index.ts
@@ -2,11 +2,11 @@ import {default as Express} from 'express'
 
 const app = Express()
 
-const port = process.env.PORT ? +process.env.PORT : 80
+const port = process.env.PORT ? Number(process.env.PORT) : 80
 const host = process.env.HOST ?? '0.0.0.0'
 
 app.use(Express.static('data'))
 
-app.listen(port, host, ( ) => {
-    console.log(`Started server at http://${host}:${port}`)
+app.listen(port, host, () => {
+  console.log(`Started server at http://${host}:${port}`)
 })
